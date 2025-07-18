1411d96ad90d36db4e7d69b500e12069b3eb1709
Tiago Ferreira <thdejesusferreira@gmail.comgit config --listgit config --listgit config --list git config --list--list>
2025-07-17 21:05:27 -0300

    add query to favorite repository

--- src/main/java/com/th/playnmovie/repository/FavoriteRepository.java
+++ src/main/java/com/th/playnmovie/repository/FavoriteRepository.java
@@ -1,9 +1,22 @@
 package com.th.playnmovie.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.th.playnmovie.model.Favorite;
+import com.th.playnmovie.model.TypeEnum;
+import com.th.playnmovie.security.model.User;
+
+import jakarta.transaction.Transactional;
 
 public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
+	
+    boolean existsByUserAndItemIdAndType(User user, Long itemId, TypeEnum type);
 
+	@Modifying
+	@Transactional
+	@Query("DELETE FROM Favorite f WHERE f.user = :user AND f.itemId = :itemId AND f.type = :type")
+	void unfavorite(@Param("user")User user,@Param("itemId")Long itemId, @Param("type")TypeEnum type);
 }
