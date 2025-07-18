68817128e9b4b4eff3f428937e9151891ba97425
Tiago Ferreira <thdejesusferreira@gmail.comgit config --listgit config --listgit config --list git config --list--list>
2025-07-18 20:33:32 -0300

    add query to review repository

--- src/main/java/com/th/playnmovie/repository/ReviewRepository.java
+++ src/main/java/com/th/playnmovie/repository/ReviewRepository.java
@@ -1,9 +1,14 @@
 package com.th.playnmovie.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.th.playnmovie.model.Review;
+import com.th.playnmovie.model.TypeEnum;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
+	
+	List<Review>findByItemIdAndType(Long itemId, TypeEnum type);
 }
