60d938ec37b42aaff20335169af9d8b3ebef233a
Tiago Ferreira <thdejesusferreira@gmail.comgit config --listgit config --listgit config --list git config --list--list>
2025-07-18 21:05:24 -0300

    add review mapper

---
+++ src/main/java/com/th/playnmovie/mapper/ReviewMapper.java
@@ -0,0 +1,29 @@
+package com.th.playnmovie.mapper;
+
+import com.th.playnmovie.dto.ReviewDto;
+import com.th.playnmovie.model.Review;
+
+public class ReviewMapper {
+	
+	public static ReviewDto toDto(Review review) {
+		ReviewDto dto = new ReviewDto();
+		dto.setComment(review.getComment());
+		dto.setId(review.getId());
+		dto.setItemId(review.getItemId());
+		dto.setNotice(review.getNotice());
+		dto.setType(review.getType());
+		dto.setUser(review.getUser());
+		return dto;
+	}
+	
+	public static Review fromDto(ReviewDto dto) {
+		Review review = new Review();
+		review.setComment(dto.getComment());
+		review.setId(dto.getId());
+		review.setItemId(dto.getItemId());
+		review.setNotice(dto.getNotice());
+		review.setType(dto.getType());
+		review.setUser(dto.getUser());
+		return review;
+	}
+}
