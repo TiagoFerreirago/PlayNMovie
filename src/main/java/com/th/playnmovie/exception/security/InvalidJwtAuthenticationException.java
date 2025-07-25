d485a9755e66803643ee1b06951c19cd48fa83fe
Tiago Ferreira <thdejesusferreira@gmail.comgit config --listgit config --listgit config --list git config --list--list>
2025-07-25 20:11:25 -0300

    add exception InvalidJwtAuthenticationException

---
+++ src/main/java/com/th/playnmovie/exception/security/InvalidJwtAuthenticationException.java
@@ -0,0 +1,19 @@
+package com.th.playnmovie.exception.security;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
+public class InvalidJwtAuthenticationException extends AuthenticationException {
+
+	private static final long serialVersionUID = 1L;
+
+	public InvalidJwtAuthenticationException(String msg) {
+		super(msg);
+	}
+	
+	public InvalidJwtAuthenticationException() {
+		super("Access denied: you do not have permission to perform this action.");
+	}
+}
