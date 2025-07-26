3dd24b2c890f8ca50cb59333120b9bc45918ad4c
Tiago Ferreira <thdejesusferreira@gmail.comgit config --listgit config --listgit config --list git config --list--list>
2025-07-25 21:13:03 -0300

    implements UserDetailsService with user loading by username

---
+++ src/main/java/com/th/playnmovie/security/service/UserService.java
@@ -0,0 +1,31 @@
+package com.th.playnmovie.security.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.th.playnmovie.security.model.User;
+import com.th.playnmovie.security.repository.UserRepository;
+
+@Service
+public class UserService implements UserDetailsService {
+  
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		User user = userRepository.findByUsername(username);
+		if(user != null) {
+			return user;
+		}
+		else {
+			throw new UsernameNotFoundException("Username "+username+" not found!");
+		}
+		
+	}
+}
+	
