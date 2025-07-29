dbd695610024dab8d502f913043a09cee6de4aea
Tiago Ferreira <thdejesusferreira@gmail.comgit config --listgit config --listgit config --list git config --list--list>
2025-07-29 20:26:54 -0300

    adds AuthController with login and refresh token endpoints

---
+++ src/main/java/com/th/playnmovie/security/controller/AuthController.java
@@ -0,0 +1,57 @@
+package com.th.playnmovie.security.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.th.playnmovie.security.service.AuthService;
+import com.th.playnmovie.security.vo.AccountCredentialsVo;
+import com.th.playnmovie.security.vo.TokenVo;
+
+import io.micrometer.common.util.StringUtils;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthController {
+	
+	@Autowired
+	private AuthService authService;
+	
+	@PostMapping("/signin")
+	public ResponseEntity<?> signin(@RequestBody AccountCredentialsVo credentials){
+		
+		if(credentialsAreInvalid(credentials)) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid client request");
+		}
+		ResponseEntity<TokenVo> vo = authService.signIn(credentials);
+		
+		return ResponseEntity.ok().body(vo);
+	}
+	@PutMapping("/refresh/{username}")
+	public ResponseEntity<?> refreshToken(@PathVariable("name")String name, @RequestHeader("Authorization") String refreshToken){
+		
+		if(paramenterAreInvalid(name, refreshToken)) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid client request");
+		}
+		ResponseEntity<TokenVo> vo = authService.refreshToken(name, refreshToken);
+		
+		return ResponseEntity.ok().body(vo);
+	}
+	
+
+	private static boolean credentialsAreInvalid(AccountCredentialsVo credentials) {
+		return (credentials == null || StringUtils.isBlank(credentials.getUsername()) || StringUtils.isBlank(credentials.getPassword()));
+	}
+	
+	private static boolean paramenterAreInvalid(String username, String password) {
+		return (StringUtils.isBlank(username) || StringUtils.isBlank(password));
+
+	}
+}
