3ef391014df0ba100d8f1b1b76e78d025f59a307
Tiago Ferreira <thdejesusferreira@gmail.comgit config --listgit config --listgit config --list git config --list--list>
2025-08-14 16:09:03 -0300

    adds Swagger/OpenAPI configuration with project information and MIT license

--- src/main/java/com/th/playnmovie/config/CustomOpenApi.java
+++ src/main/java/com/th/playnmovie/config/CustomOpenApi.java
@@ -1,20 +1,25 @@
 package com.th.playnmovie.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 
+@Configuration
 public class CustomOpenApi {
 
+	@Bean
 	OpenAPI customOpenApi() {
 		return new OpenAPI()
 				.info(new Info()
-						.title(null)
-						.description(null)
-						.version(null)
-						.termsOfService(null)
+						.title("PlayNmovie API")
+						.description("API for managing movies, games, favorites, and reviews. Personal project for portfolio.")
+						.version("1.0.0")
+						.termsOfService("https://github.com/TiagoFerreirago/PlayNMovie")
 						.license(new License()
-								.name(null)
-								.url(null)));
+								.name("MIT License")
+                    .url("https://opensource.org/licenses/MIT")));
 	}
 }
